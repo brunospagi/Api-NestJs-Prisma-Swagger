8e04ef0b1d03b936f302d9ad3f21b15ce37dbcaf
Bruno Gabriel <brunospagi@gmail.com>
2023-09-02 11:34:02 -0300

    Retirado espaço VsCode

--- src/funcionario/funcionario.module.ts
+++ src/funcionario/funcionario.module.ts
@@ -3,7 +3,6 @@ import { FuncionarioService } from './funcionario.service';
 import { FuncionarioController } from './funcionario.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
-
 @Module({
   controllers: [FuncionarioController],
   providers: [FuncionarioService],
