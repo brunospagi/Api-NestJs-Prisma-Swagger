90dd976d23dd2b84a1f18609c5cc39515a836bab
Bruno Gabriel <brunospagi@gmail.com>
2023-09-02 11:33:49 -0300

    Adicionado Sumario ao Swagger de Cada Operação

--- src/funcionario/funcionario.controller.ts
+++ src/funcionario/funcionario.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { FuncionarioService } from './funcionario.service';
 import { CreateFuncionarioDto } from './dto/create-funcionario.dto';
 import { UpdateFuncionarioDto } from './dto/update-funcionario.dto';
@@ -10,26 +19,34 @@ export class FuncionarioController {
   constructor(private readonly funcionarioService: FuncionarioService) {}
 
   @Post('criar')
+  @ApiOperation({ summary: 'Criar um Funcionario' })
   create(@Body() createFuncionarioDto: CreateFuncionarioDto) {
     return this.funcionarioService.create(createFuncionarioDto);
   }
 
   @Get('listar-todos')
+  @ApiOperation({ summary: 'Listar Todos Os Funcionarios' })
   findAll() {
     return this.funcionarioService.findAll();
   }
 
   @Get('listar/:id')
+  @ApiOperation({ summary: 'Buscar Funcionario Pelo ID' })
   findOne(@Param('id') id: string) {
     return this.funcionarioService.findOne(+id);
   }
 
   @Patch('atualizar/:id')
-  update(@Param('id') id: string, @Body() updateFuncionarioDto: UpdateFuncionarioDto) {
+  @ApiOperation({ summary: 'Atualizar Um Funcionario Pelo ID' })
+  update(
+    @Param('id') id: string,
+    @Body() updateFuncionarioDto: UpdateFuncionarioDto,
+  ) {
     return this.funcionarioService.update(+id, updateFuncionarioDto);
   }
 
   @Delete('delatar/:id')
+  @ApiOperation({ summary: 'Deletar Funcionario Pelo ID' })
   remove(@Param('id') id: string) {
     return this.funcionarioService.remove(+id);
   }
